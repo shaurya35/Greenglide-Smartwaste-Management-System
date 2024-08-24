f5356f1ca847aec532d0d4d8f63b05007f49d36a
OmShankarDeshmukh01 <omdeshmukhstudy@gmail.com>
2024-08-24 20:19:54 +0530

    added controllers and routes

--- server/index.js
+++ server/index.js
@@ -2,3 +2,31 @@ const express = require('express');
 const mongoose = require("mongoose");
 require('dotenv').config();
 const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+
+
+
+// Log all requests
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+const userAuthRoutes = require("./routes/userAuthRoutes.js");
+
+// Base route
+app.get("/", (req, res) => {
+    res.json('/ route here');
+  });
+
+  app.use("/api/auth/user", userAuthRoutes);
+  app.use("/api/auth/employee", userAuthRoutes);
+  app.use("/api/auth/staff", userAuthRoutes);
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to DB and running on port http://localhost:${process.env.PORT}/`);
+    });
+  });
